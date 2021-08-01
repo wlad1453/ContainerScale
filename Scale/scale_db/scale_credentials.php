3a9d85c6129ebef458986e7e055518bef19bb18b
Vladimir Filippov <vfabian@mail.ru>
2021-08-01 20:15:12 +0300

    Update scale_credentials.php

--- Scale/scale_db/scale_credentials.php
+++ Scale/scale_db/scale_credentials.php
@@ -2,7 +2,7 @@
 
 $cred_db = array(	"server"=>"localhost", 
 			"user"  =>"u486738398_scale", 
-			"pwd"   =>"Container100Scale", 
+			"pwd"   =>"Cxxxxxx1xxSxxxx", 
 			"db"    =>"u486738398_scale_DB");
 
 	
